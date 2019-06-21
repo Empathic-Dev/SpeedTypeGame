882d4f291b2fe65a5a6f03e9ccea7fd7881b8f85
Deja <theempathicdev@gmail.com>
2019-06-21 17:51:34 -0400

    started js

--- game.js
+++ game.js
@@ -1,1 +1,48 @@
-console.log('Stay Focused, You Got This! I love you!!');
+window.addEventListener('load', init);
+
+// global var
+let time = 5;
+let score = 0;
+let isPlaying; // when the player is typing: true, when the player wins/looses: false
+
+// dom elements
+const currentWord = document.querySelector('#current-word');
+const wordInput = document.querySelector('#word-input');
+const message = document.querySelector('#message');
+const timeDisplay = document.querySelector('#time');
+const scoreDisplay = document.querySelector('#score');
+const seconds = document.querySelector('#seconds');
+
+const words = [
+  'hat',
+  'river',
+  'lucky',
+  'statue',
+  'generate',
+  'stubborn',
+  'cocktail',
+  'runaway',
+  'joke',
+  'developer',
+  'establishment',
+  'hero',
+  'javascript',
+  'nutrition',
+  'revolver',
+  'echo',
+  'siblings',
+  'investigate',
+  'horrendous',
+  'symptom',
+  'laughter',
+  'magic',
+  'master',
+  'space',
+  'definition'
+];
+
+//Initialize Game
+
+function init(){
+  console.log('Stay Focused, You Got This! I love you!!');  
+}
